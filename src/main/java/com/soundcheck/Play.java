80e2dbdc25e0cd12062befc96c158c04f1f0b323
aniruddhas435 <aniruddhas435@gmail.com>
2025-12-24 20:30:52 +0530

    Major upgrade - own bean init framework

--- src/main/java/com/soundcheck/Play.java
+++ src/main/java/com/soundcheck/Play.java
@@ -7,6 +7,6 @@ public class Play {
   public static void main(String[] args) {
 //    SoundCheckApplication.launch("com.soundcheck", Launcher::play,
 //        "src/test/raag_files/miyan_ki_malhar/test3/mkm.raag", true);
-		SoundCheckApplication.launch("com.soundcheck", Launcher::play, args[0], Boolean.parseBoolean(args[2]));
+		SoundCheckApplication.launch("com.soundcheck", Launcher::play, args[0], Boolean.parseBoolean(args[1]));
   }
 }
